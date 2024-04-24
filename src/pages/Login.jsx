73e664f2af69b71ddaead25c81287fcc205a1c34
rishik puneet m <rishikpuneetm@gmail.com>
2024-04-24 13:24:17 +0530

    frontend login console.log changes done...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,42 +2,41 @@ import axios from "axios";
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function Login() {
     const [user, setUser] = useState({});
 
     const loginUser = async (e) => {
         try {
-            const response = await toast
-                .promise(
-                    axios.post(
-                        "https://chatback-ryc1.onrender.com/api/v1/user/login",
-                        user
-                    ),
-                    {
-                        pending: "Logging In User",
-                        success: "User Logged In Successfully",
-                        error: "Something went wrong in the logging user",
-                    }
-                )
-                .then((response) => {
-                    console.log("response", response);
-                })
-                .then((response) => {
-                    const token = response.data.token;
-                    console.log("token: ", token);
-                    console.log("response", response);
-                    console.log("response.data: ", response.data);
-                    console.log("response.data.token: ", response.data.token);
-                    localStorage.setItem("token", token); // Store in local storage
-                    sessionStorage.setItem("token", token);
-
-                    // Set token in headers for subsequent requests
-                    axios.defaults.headers.common[
-                        "Authorization"
-                    ] = `Bearer ${token}`;
-                });
+            await toast.promise(loginRequest(), {
+                pending: "Logging In User",
+                success: "User Logged In Successfully",
+                error: "Something went wrong in the logging user",
+            });
         } catch (error) {
             console.log("something went wrong in the logging user", error);
+        }
+    };
+
+    const loginRequest = async () => {
+        try {
+            const response = await axios.post(
+                "https://chatback-ryc1.onrender.com/api/v1/user/login",
+                user
+            );
+
+            const token = response.data.token;
+            console.log("token: ", token);
+            console.log("response", response);
+            console.log("response.data: ", response.data);
+            console.log("response.data.token: ", response.data.token);
+            localStorage.setItem("token", token); // Store in local storage
+            sessionStorage.setItem("token", token);
+
+            // Set token in headers for subsequent requests
+            axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        } catch (error) {
+            throw error;
         }
     };
 
